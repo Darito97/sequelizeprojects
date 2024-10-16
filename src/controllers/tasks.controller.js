e650a8250be2744d473f2153031e76c4808d1fc5
David Alonso <davidalonso991@gmail.com>
2024-10-16 09:31:10 -0600

    Agregado de update, delete y findById a task

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -13,11 +13,11 @@ const getTasks = async (req, res) => {
 
 const createTask = async (req, res) => {
   try {
-    let { name, done, projectid } = req.body;
+    let { name, done, projectId } = req.body;
     const newTask = await Task.create({
       name,
       done,
-      projectId: projectid,
+      projectId: projectId,
     });
     res.json(newTask);
   } catch (err) {
@@ -27,10 +27,66 @@ const createTask = async (req, res) => {
   }
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let { name, done, projectId } = req.body;
+    const taskUpdated = await Task.update(
+      {
+        name,
+        done,
+        projectId,
+      },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+    res.json({
+      msg: "Tarea actualizada correctamente",
+      taskUpdated,
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: "Hubo un error al actualizar la tarea",
+    });
+  }
+};
 
-const deleteTask = async (req, res) => {};
+const deleteTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const taskDeleted = await Task.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.json({
+      msg: "Tarea eliminada correctamente",
+      taskDeleted,
+    });
+  } catch (err) {
+    res.status(500).json({
+      msg: "Hubo un error al eliminar la tarea",
+    });
+  }
+};
 
-const getTask = async (req, res) => {};
+const getTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const task = await Task.findOne({
+      where: {
+        id: id,
+      },
+    });
+    res.json(task);
+  } catch (err) {
+    res.status(500).json({
+      msg: "Hubo un error al obtener la tarea",
+    });
+  }
+};
 
 export { getTasks, createTask, updateTask, deleteTask, getTask };
